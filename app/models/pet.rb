fee0ff1d940abf97c1b2b178a8f434de92be4df1
DOROTHY2322 <kimelidorothy@gmail.com>
2023-03-01 14:18:39 +0300

    adding files

---
+++ app/models/pet.rb
@@ -0,0 +1,9 @@
+class Pet < ActiveRecord::Base
+    belongs_to :user
+    
+    validates :name, presence: true
+    validates :breed, presence: true
+    validates :age, presence: true, numericality: { only_integer: true }
+    validates :img_url, presence: true
+  end
+  
