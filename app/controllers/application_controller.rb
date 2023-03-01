ddea1789192b0d6fc34bb00ffa50808893fe3949
DOROTHY2322 <kimelidorothy@gmail.com>
2023-03-01 15:58:08 +0300

    post request to add a the pets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
     set default_content_type: "application/json"
 
@@ -8,7 +9,20 @@ class ApplicationController < Sinatra::Base
         pets = Pet.all
         # return a JSON response with an array of all the pet data
         pets.to_json
-  
+
       end
-     
+      # Post request to enable a user to add a new pet
+  post '/pets' do
+    # Parse the data sent in the POST request
+    pet_data = JSON.parse(request.body.read)
+    name = pet_data['name']
+    breed = pet_data['breed']
+    age = pet_data['age']
+
+    # Create a new pet record in the database
+    pet = Pet.create(name: name, breed: breed, age: age)
+
+    # Return the details of the newly created pet
+    pet.to_json
+  end
 end
