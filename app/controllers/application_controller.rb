5ac8a905745d735f4d5f99b760350d3677f2e838
DOROTHY2322 <kimelidorothy@gmail.com>
2023-03-01 15:45:49 +0300

    get request for all the pets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < Sinatra::Base
     set default_content_type: "application/json"
+
+    #get request to access  all the pets
+    #GET request to get all the pets
+    get '/pets' do
+        # get all the pets from the database
+        pets = Pet.all
+        # return a JSON response with an array of all the pet data
+        pets.to_json
+  
+      end
+     
 end
